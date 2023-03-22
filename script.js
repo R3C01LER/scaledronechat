8f96ea25f24632ae31781a50833ab2fa8582ee8a
JuliansABot <122099740+jsan3568@users.noreply.github.com>
2023-03-22 08:25:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const CLIENT_ID = 'zVzKAkHh86etWVVd';
-const ownerlist=["Matteo","dragonmaster73101"];
-const adminlist=["Nate","Pax","Fedex Driver DragonJR","Bothacker1115","davey","dragonjr","Joey - Let's Ship out Your Order!","davey2"];
-const coadminlist=["Yoga","mason","KAISER WILHELM II"," C@MDEN","King cupcake lll"];
-const fedex=["testname"];
-const bot=["Bothacker1115"];
+const ownerlist=["J","D"];
+const adminlist=["Blake"];
+const coadminlist=["Wyatt"];
 const youreverydaydavey=["davey2"];
 //PUT BANNED NAMES IN BEGIN.HTML AND ADMIN NAMES IN CHANGEUSERNAME.HTML TOO
 function get_cookie(cookie_name) { const value = "; " + document.cookie; const parts = value.split("; " + cookie_name + "="); if (parts.length === 2) return parts.pop().split(";").shift(); }
